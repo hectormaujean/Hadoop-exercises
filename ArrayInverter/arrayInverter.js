2df7f833a6069e3afee3c0b05b76a27a06a0bea2
Unknown <hectormaujean@gmail.com>
2017-10-03 21:50:14 +0200

    Commited array inverter code

---
+++ ArrayInverter/arrayInverter.js
@@ -0,0 +1,13 @@
+var items = [
+  [1, 2, 3, 4, 5, 6, 7],
+  [8],
+  [9],
+  [10]
+];
+console.log(items[0][0]);
+console.log(items);
+
+function myFunction() {
+    x = document.getElementById("demo")
+    x.innerHTML = numbers.map(Math.sqrt);
+}
