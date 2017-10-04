07c92b785944b9ffaae0a0bbad21705d6fca0e92
Unknown <hectormaujean@gmail.com>
2017-10-04 19:31:29 +0200

    Wrong file was commited previously

--- ArrayInverter/arrayInverter.js
+++ ArrayInverter/arrayInverter.js
@@ -1,13 +1,42 @@
 var items = [
   [1, 2, 3, 4, 5, 6, 7],
-  [8],
-  [9],
-  [10]
+  [8, 60, 40, 51],
+  [9, 21, 32, 75],
+  [10, 49, 12]
 ];
-console.log(items[0][0]);
-console.log(items);
 
-function myFunction() {
-    x = document.getElementById("demo")
-    x.innerHTML = numbers.map(Math.sqrt);
+for (var i = 0; i < items.length; i++) {
+	console.log(items[i]);
+}
+console.log('\n')
+
+function transpose(array) {
+    return Object.keys(array[0]).map(function(column) {
+        return array.map(function(row) { return row[column]; });
+    });
+}
+
+var remove = function (array) {
+  var result = [];
+
+  array.forEach(function (item) {
+    if (Array.isArray(item) && item.length!=0) {
+      // Item is a nested array, go one level deeper recursively
+      result.push(remove(item));
+    }
+    else if (typeof item !== 'undefined') {
+      result.push(item);
+    }
+  });
+
+  return result;
+};
+
+items = transpose(items);
+
+items = remove(items);
+
+
+for (var i = 0; i < items.length; i++) {
+	console.log(items[i]);
 }
